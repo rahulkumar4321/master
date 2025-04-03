7afbe60c5c2929cdd56abd766cc4b575882c08d7
rahulkumar4321 <3525rahulkumar@gmail.com>
2025-04-03 23:57:20 +0530

    add logo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import logo from "./pnblogo.png"
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src="https://via.placeholder.com/100" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
       <h1 className="title">Header</h1>
       <h2>my name is Rahul Kumar</h2>
